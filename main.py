5aa90b6bb74d63dea804a9d452f451a5d680dfff
Anton Coding <43090912+AntonIT99@users.noreply.github.com>
2023-01-29 18:46:37 +0100

    Fetching complete instagram feed and display date

--- main.py
+++ main.py
@@ -1,23 +1,32 @@
+import functools
+import locale
 import traceback
+from datetime import date, datetime, timedelta
+from time import sleep
 from typing import Optional
 
 import discord
 import requests
+import typing
 from discord.ext import commands, tasks
 from parse import parse
 
+# Post date limit, prevent the bot to post too old images from instagram, will not affect updating of already posted images
+POST_DATE_LIMIT = date.today() - timedelta(days=60)
 # Should the bot update existing posts to get the latest number of likes and comments
 UPDATE_POSTS = True
-# Display debug log when updating a post to track differences
-UPDATE_DEBUG_LOG = True
+# Display additional logs
+DEBUG_LOG = True
 # Token of the bot
 TOKEN = 'enter your bot token here' # enter your bot token here
 # instagram account names
 INSTAGRAM_ACCOUNTS = ['instagram', 'instagramforbusiness', 'meta']
 # channel id where the images are posted
 CHANNEL_ID = 1234567890 # replace by your channel id
-# limit of the message history read when checking if latest instagram publications have already been posted in the channel
-MESSAGE_HISTORY_LIMIT = 100
+# maximal number of images that should be fetched from each instagram account, set to None for no limit
+FETCHING_LIMIT = None
+# limit of the message history read when checking if latest instagram publications have already been posted in the channel, set to None for no limit
+MESSAGE_HISTORY_LIMIT = None
 # color of the messages
 COLOR = 0x05c0ec
 # time in seconds after which an update check is performed
@@ -26,17 +35,17 @@ UPDATE_INTERVAL = 3600
 TIMEOUT = 5
 # header for sending requests to Instagram
 HEADER = {
-        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
-        "Accept-Encoding": "de,en-US;q=0.7,en;q=0.3",
-        "Connection": "keep-alive",
-        "Cookie": "enter your cookie here", # enter your cookie here
-        "Host": "www.instagram.com",
-        "Sec-Fetch-Dest": "document",
-        "Sec-Fetch-Mode": "navigate",
-        "Sec-Fetch-Site": "cross-site",
-        "TE": "trailers",
-        "Upgrade-Insecure-Requests": "1",
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:108.0) Gecko/20100101 Firefox/108.0"
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+    "Accept-Encoding": "de,en-US;q=0.7,en;q=0.3",
+    "Connection": "keep-alive",
+    "Cookie": "csrftoken=enter your cookie here", # enter your cookie from your web browser here
+    "Host": "www.instagram.com",
+    "Sec-Fetch-Dest": "document",
+    "Sec-Fetch-Mode": "navigate",
+    "Sec-Fetch-Site": "cross-site",
+    "TE": "trailers",
+    "Upgrade-Insecure-Requests": "1",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:108.0) Gecko/20100101 Firefox/108.0"
 }
 
 bot = commands.Bot(command_prefix="$", intents=discord.Intents.all())
@@ -58,13 +67,14 @@ class Author:
 
 class InstagramPost:
 
-    def __init__(self, author: Author, image_code: str, image_url: str, description: str, likes: int, comments: int):
+    def __init__(self, author: Author, image_code: str, image_url: str, description: str, likes: int, comments: int, post_date: date):
         self.author = author
         self.image_code = str(image_code)
         self.image_url = str(image_url)
         self.description = str(description)
         self.likes = int(likes)
         self.comments = int(comments)
+        self.post_date = post_date
 
     def __eq__(self, other):
         return (isinstance(other, InstagramPost) and
@@ -73,7 +83,8 @@ class InstagramPost:
                 self.image_url == other.image_url and
                 self.description == other.description and
                 self.likes == other.likes and
-                self.comments == other.comments)
+                self.comments == other.comments and
+                self.post_date == other.post_date)
 
 
 @bot.event
@@ -96,32 +107,27 @@ async def ping(ctx):
 @bot.command()
 async def update(ctx):
     print("Force updating")
-    await update_from_instagram(bot.get_channel(CHANNEL_ID))
+    await update_from_instagram()
 
 
 @tasks.loop(seconds=UPDATE_INTERVAL)
 async def updater():
-    await update_from_instagram(bot.get_channel(CHANNEL_ID))
+    await update_from_instagram()
 
 
-async def update_from_instagram(channel: discord.channel):
-    last_published_posts = await read_published_messages(channel)
+async def update_from_instagram():
+    messages_of_instagram_posts = await read_published_messages()
     posts_from_all_authors = {}
-    for username in INSTAGRAM_ACCOUNTS:
-        author = get_user_data(username)
-        if author is not None:
-            posts_from_all_authors[username] = get_last_posts_from_instagram(author)
-        else:
-            print(username + ": Failed to update")
+    await run_blocking(fetch_posts_from_instagram, posts_from_all_authors)
 
     for username in posts_from_all_authors:
         new_images = 0
         updated_images = 0
         for code in posts_from_all_authors[username]:
-            if code not in [post.image_code for post in last_published_posts]:
-                new_images = await post_image(channel, posts_from_all_authors[username][code], new_images)
+            if code not in messages_of_instagram_posts.keys():
+                new_images = await post_image(posts_from_all_authors[username][code], new_images)
             elif UPDATE_POSTS:
-                message = await get_published_message_from_image_code(channel, code)
+                message = messages_of_instagram_posts[code]
                 if message is not None:
                     updated_images = await update_image(message, posts_from_all_authors[username][code], updated_images)
         if new_images > 0:
@@ -132,10 +138,26 @@ async def update_from_instagram(channel: discord.channel):
             print("[{}]: {} image{} updated in discord".format(username, updated_images, "s" if updated_images > 1 else ""))
 
 
+# Runs a blocking function in a non-blocking way
+async def run_blocking(blocking_func: typing.Callable, *args, **kwargs) -> typing.Any:
+    func = functools.partial(blocking_func, *args, **kwargs)
+    return await bot.loop.run_in_executor(None, func)
+
+
+def fetch_posts_from_instagram(posts_from_all_authors: dict):
+    for username in INSTAGRAM_ACCOUNTS:
+        user_id, author = get_user_data(username)
+        if user_id is not None and author is not None:
+            posts_from_all_authors[username] = get_posts_from_instagram(user_id, author)
+        else:
+            print(username + ": Failed to update: can not retrieve account data")
+
+
 SHORTCODE_URL = "https://www.instagram.com/p/{}/"
 AUTHOR_URL = "https://www.instagram.com/{}/"
 TITLE_STR = "Neuer Beitrag von @{}"
-FOOTER_STR = "Gefällt {} Mal\n{} Kommentare"
+FOOTER_STR = "Gefällt {} Mal\n{} Kommentare\n{}"
+DATE_FORMAT = "%B %d, %Y"
 
 
 # generate an embed from an InstagramPost
@@ -148,107 +170,151 @@ def create_embed(post: InstagramPost) -> discord.Embed:
                      url=AUTHOR_URL.format(post.author.username),
                      icon_url=post.author.profile_pic_url)
     embed.set_image(url=post.image_url)
-    embed.set_footer(text=FOOTER_STR.format(post.likes, post.comments))
+    embed.set_footer(text=FOOTER_STR.format(post.likes, post.comments, post.post_date.strftime(DATE_FORMAT)))
     return embed
 
 
-# read all published messages in the channel and return the corresponding list of InstagramPost
-async def read_published_messages(channel: discord.channel) -> list:
-    last_published_posts = []
-    async for message in channel.history(limit=MESSAGE_HISTORY_LIMIT):
-        post = get_post_from_discord_message(message)
-        if post is not None:
-            last_published_posts.append(post)
-    return last_published_posts
+# read all published messages in the channel
+async def read_published_messages() -> dict:
+    messages = {}
+    async for message in bot.get_channel(CHANNEL_ID).history(limit=MESSAGE_HISTORY_LIMIT):
+        if message.author.id == bot.user.id:
+            post = get_post_from_discord_message(message)
+            if post is not None:
+                messages[post.image_code] = message
+    return messages
 
 
 # return an InstagramPost from a discord message
-def get_post_from_discord_message(message: discord.message) -> Optional[InstagramPost]:
+def get_post_from_discord_message(message: discord.Message) -> Optional[InstagramPost]:
     if len(message.embeds) > 0:
         embed = message.embeds[0]
-        username = parse(AUTHOR_URL, str(embed.author.url))
         shortcode = parse(SHORTCODE_URL, str(embed.url))
-        if username is not None and shortcode is not None:
-            username = username[0]
+        if shortcode is not None:
             shortcode = shortcode[0]
+            username = parse(AUTHOR_URL, str(embed.author.url))
+            if username is not None:
+                username = username[0]
+            else:
+                username = ""
             description = str(embed.description or '')
             image_url = str(embed.image.url or '')
             author_name = str(embed.author.name or '')
             author_icon_url = str(embed.author.icon_url or '')
             likes = 0
             comments = 0
+            post_date = date(2000, 1, 1)
             footer = parse(FOOTER_STR, str(embed.footer.text))
             if footer is not None:
                 likes = int(footer[0])
                 comments = int(footer[1])
+                post_date = datetime.strptime(str(footer[2]), DATE_FORMAT).date()
             author = Author(username, author_name, author_icon_url)
-            return InstagramPost(author, shortcode, image_url, description, likes, comments)
-    return None
-
-
-# get discord message with embed image from the image code
-async def get_published_message_from_image_code(channel: discord.channel, code: str) -> discord.message:
-    async for message in channel.history(limit=MESSAGE_HISTORY_LIMIT):
-        if len(message.embeds) > 0:
-            embed = message.embeds[0]
-            shortcode = parse(SHORTCODE_URL, str(embed.url))
-            if shortcode is not None:
-                if code == shortcode[0]:
-                    return message
+            return InstagramPost(author, shortcode, image_url, description, likes, comments, post_date)
+        else:
+            return None
     return None
 
 
 # post embedded image to discord channel
-async def post_image(channel: discord.channel, post: InstagramPost, counter: int):
-    await channel.send(embed=create_embed(post))
-    return counter + 1
-
-
-# update existing embedded image
-async def update_image(message: discord.message, new_post: InstagramPost, counter: int):
-    existing_post = get_post_from_discord_message(message)
-    if new_post != existing_post:
-        new_embed = create_embed(new_post)
-        if UPDATE_DEBUG_LOG:
-            print_update_log(new_post, existing_post)
-        await message.edit(embed=new_embed)
-        counter += 1
+async def post_image(post: InstagramPost, counter: int) -> int:
+    if post.post_date >= POST_DATE_LIMIT:
+        await bot.get_channel(CHANNEL_ID).send(embed=create_embed(post))
+        return counter + 1
     return counter
 
 
-# return an Author from the user data of the specified account
-def get_user_data(username: str) -> Optional[Author]:
+# update existing embedded image
+async def update_image(message: discord.Message, new_post: InstagramPost, counter: int) -> int:
+    existing_post = get_post_from_discord_message(message)
+    if new_post != existing_post:
+        new_embed = create_embed(new_post)
+        if DEBUG_LOG:
+            print_update_log(new_post, existing_post)
+        if message.author.id == bot.user.id:
+            await message.edit(embed=new_embed)
+            counter += 1
+    return counter
+
+
+# return an Author and its user id from the user data of the specified account
+def get_user_data(username: str) -> (Optional[str], Optional[Author]):
     response = requests.get("https://www.instagram.com/{}/?__a=1&__d=dis".format(username), headers=HEADER, timeout=TIMEOUT)
     try:
         user_data = response.json()["graphql"]["user"]
-        return Author(username, user_data["full_name"], user_data["profile_pic_url"])
+        return user_data["id"], Author(username, user_data["full_name"], user_data["profile_pic_url_hd"])
     except Exception:
         traceback.print_exc()
-        return None
+        return None, None
 
 
 # return a dict of InstagramPost from the data of the latest images available from the specified Author
-def get_last_posts_from_instagram(author: Author) -> Optional[dict]:
-    response = requests.get("https://www.instagram.com/{}/?__a=1&__d=dis".format(author.username), headers=HEADER, timeout=TIMEOUT)
+def get_posts_from_instagram(user_id: str, author: Author) -> dict:
+    images = {}
+
     try:
-        images = {}
-        images_list = response.json()["graphql"]["user"]["edge_owner_to_timeline_media"]["edges"]
+        if DEBUG_LOG:
+            print("[{}]: fetching latest images...".format(author.username))
+        json = get_json_from_graphql_query(user_id)
+        images_count = int(json["data"]["user"]["edge_owner_to_timeline_media"]["count"])
+        if FETCHING_LIMIT is not None and images_count > FETCHING_LIMIT:
+            images_count = FETCHING_LIMIT
+        step = len(json["data"]["user"]["edge_owner_to_timeline_media"]["edges"])
+    except Exception:
+        traceback.print_exc()
+        return images
+
+    fetched_images = 0
+
+    for i in range(0, images_count, step):
+
+        end_cursor = str(json["data"]["user"]["edge_owner_to_timeline_media"]["page_info"]["end_cursor"])
+        images_list = json["data"]["user"]["edge_owner_to_timeline_media"]["edges"]
         for image_data in images_list:
             image_code = str(image_data["node"]["shortcode"])
             image_url = str(image_data["node"]["display_url"])
             description = ""
-            desc_txt = image_data["node"]["edge_media_to_caption"]["edges"]
-            if len(desc_txt) > 0:
-                if "node" in desc_txt[0].keys():
-                    if "text" in desc_txt[0]["node"].keys():
-                        description = str(desc_txt[0]["node"]["text"])
-            likes = int(image_data["node"]["edge_liked_by"]["count"])
+            caption_data = image_data["node"]["edge_media_to_caption"]["edges"]
+            if len(caption_data) > 0:
+                if "node" in caption_data[0].keys():
+                    if "text" in caption_data[0]["node"].keys():
+                        description = str(caption_data[0]["node"]["text"])
+            likes = int(image_data["node"]["edge_media_preview_like"]["count"])
             comments = int(image_data["node"]["edge_media_to_comment"]["count"])
-            images[image_code] = InstagramPost(author, image_code, image_url, description, likes, comments)
-        return images
-    except Exception:
-        traceback.print_exc()
-        return None
+            timestamp = int(image_data["node"]["taken_at_timestamp"])
+            images[image_code] = InstagramPost(author, image_code, image_url, description, likes, comments, date.fromtimestamp(timestamp))
+            fetched_images += 1
+
+        if fetched_images != images_count:
+            try:
+                if DEBUG_LOG:
+                    print("[{}]: fetching images {} to {}...".format(author.username, fetched_images,
+                                                                     fetched_images + step - 1 if fetched_images + step - 1 < images_count else images_count - 1))
+                json = get_json_from_graphql_query(user_id, end_cursor)
+            except Exception:
+                traceback.print_exc()
+                print("[{}]: failed to fetching images, retrying in 5s...".format(author.username))
+                sleep(5)
+                try:
+                    print("[{}]: retrying to fetch images {} to {}...".format(author.username,
+                                                                              fetched_images, fetched_images + step - 1 if fetched_images + step - 1 < images_count else images_count - 1))
+                    json = get_json_from_graphql_query(user_id, end_cursor)
+                except Exception:
+                    traceback.print_exc()
+                    if DEBUG_LOG:
+                        print("[{}]: abort the fetching process".format(author.username))
+                    break
+
+    print("[{}]: {} images fetched from instagram".format(author.username, fetched_images))
+    return images
+
+
+def get_json_from_graphql_query(user_id: str, end_cursor=""):
+    if end_cursor:
+        response = requests.get("https://www.instagram.com/graphql/query/?query_id=17888483320059182&id={}&first=12&after={}".format(user_id, end_cursor), headers=HEADER, timeout=TIMEOUT)
+    else:
+        response = requests.get("https://www.instagram.com/graphql/query/?query_id=17888483320059182&id={}&first=12".format(user_id), headers=HEADER, timeout=TIMEOUT)
+    return response.json()
 
 
 def print_update_log(new: InstagramPost, old: InstagramPost):
@@ -268,7 +334,10 @@ def print_update_log(new: InstagramPost, old: InstagramPost):
         print("[{}] {}: {} likes changed to {} likes".format(new.author.username, new.image_code, old.likes, new.likes))
     if new.comments != old.comments:
         print("[{}] {}: {} comments changed to {} comments".format(new.author.username, new.image_code, old.comments, new.comments))
+    if new.post_date != old.post_date:
+        print("[{}] {}: post_date {} changed to {}".format(new.author.username, new.image_code, old.post_date, new.post_date))
 
 
 if __name__ == "__main__":
+    locale.setlocale(locale.LC_TIME, "de_DE")  # German locale
     bot.run(TOKEN)
